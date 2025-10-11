a1b05680f27576b53441c56900a9463cf0ba61d2
who-stole-my-username <exeteusog@gmail.com>
2025-10-11 10:51:46 +0200

    Fixed the Strings

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pymongo
-
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 db = client["kinofilme"]
@@ -10,6 +9,17 @@ print("|            Was möchten sie tun?            |")
 print("+---+----------------++---+------------------+")
 print("| 1 | Daten abfragen || 2 | Daten bearbeiten |")
 print("+---+----------------++---+------------------+")
-choose = input("Auswahl: ")
+
+while True:
+  choose = input("Auswahl: ")
+
+  if choose == "1":
+    print("1")
+  elif choose == "2":
+    print("2")
+  elif choose == "^C":
+    exit
+  else:
+    print("Keine gültige Auswahl!")
 
 client.close()
