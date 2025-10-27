97ee07041a9a217cc26896ffad2a0a6aeeb13c67
who-stole-my-username <exeteusog@gmail.com>
2025-10-27 18:56:19 +0100

    Added the edit section (it was a lot of copy pasting cause i didnt make functions earlyer

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pymongo
 import curses
-import re
 from curses import wrapper
 from curses.textpad import Textbox, rectangle
 
 def dmain(stdscr, COLOR_GREEN):
     stdscr.clear()
     y, x = stdscr.getmaxyx()
-    stdscr.addstr(2, 4, "DVD Samlung Projekt", COLOR_GREEN | curses.A_UNDERLINE)
+    stdscr.addstr(2, 4, "DVD Sammlung", COLOR_GREEN | curses.A_UNDERLINE)
     stdscr.addstr(4, 4, "Was wollen sie tun?", COLOR_GREEN)
     stdscr.addstr(y - 8, 4, "Q: Zurück -> funktioniert immer", COLOR_GREEN | curses.A_DIM)
     stdscr.addstr(y - 4, 4, "1: Nach Filmen suchen", COLOR_GREEN)
@@ -35,7 +34,7 @@ def main(stdscr):
   try:
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["kinofilme"]
-    collection = db["dvd_samlung"]
+    collection = db["dvd_sammlung"]
 
     dmain(stdscr, COLOR_GREEN)
 
@@ -51,7 +50,7 @@ def main(stdscr):
 
       if state == "main":
         if key == "1":
-          state = "search"
+          state = "suchen"
           stdscr.clear()
           y, x = stdscr.getmaxyx()
           stdscr.addstr(2, 4, "Nach Filmen suchen", COLOR_GREEN | curses.A_UNDERLINE)
@@ -73,6 +72,22 @@ def main(stdscr):
         elif key == "2":
           state = "bearbeiten"
           stdscr.clear()
+          y, x = stdscr.getmaxyx()
+          stdscr.addstr(2, 4, "Filme bearbeiten", COLOR_GREEN | curses.A_UNDERLINE)
+          stdscr.addstr(4, 4, "Was wollen sie bearbeiten?", COLOR_GREEN)
+          stdscr.addstr(y - 8, 4, "1: Titel", COLOR_GREEN)
+          stdscr.addstr(y - 8, x - 25, "2: Art", COLOR_GREEN)
+          stdscr.addstr(y - 6, 4, "3: Jahr", COLOR_GREEN)
+          stdscr.addstr(y - 6, x - 25, "4: Regisseur", COLOR_GREEN)
+          stdscr.addstr(y - 4, 4, "5: Schauspieler", COLOR_GREEN)
+          stdscr.addstr(y - 4, x - 25, "6: Bewertungen", COLOR_GREEN)
+          stdscr.addstr(y - 2, 4, "7: Mindestalter", COLOR_GREEN)
+          stdscr.addstr(y - 2, x - 25, "8: Bemerkungen", COLOR_GREEN)
+
+          for i in range(x):
+            stdscr.addstr(y - 10, i, "-", COLOR_GREEN)
+            i = i + 1
+
         elif key == "3":
           state = "einfügen"
           stdscr.clear()
@@ -80,7 +95,7 @@ def main(stdscr):
           state = "löschen"
           stdscr.clear()
 
-      elif state == "search":
+      elif state == "suchen":
         if key == "1":
           option = "name"
         elif key == "2":
@@ -140,9 +155,67 @@ def main(stdscr):
 
           result.refresh(0, 0, 9, 5, y - 3, x - 7)
 
+      elif state == "bearbeiten":
+        if key == "1":
+          option = "name"
+        elif key == "2":
+          option = "art"
+        elif key == "3":
+          option = "jahr"
+        elif key == "4":
+          option = "regisseur"
+        elif key == "5":
+          option = "schauspieler"
+        elif key == "6":
+          option = "rating"
+        elif key == "7":
+          option = "min_alter"
+        elif key == "8":
+          option = "bemerkung"
 
+        if option:
+          stdscr.clear()
+          stdscr.addstr(2, 4, "Filme bearbeiten", COLOR_GREEN | curses.A_UNDERLINE)
+          stdscr.addstr(5, 5, "1: Filmtitel des zu bearbeitenden Filmes:", COLOR_GREEN)
+          stdscr.addstr(9, 5, f"2: Neue(n/s) {option} eingeben:", COLOR_GREEN)
+          stdscr.addstr(2, 24, option, COLOR_GREEN | curses.A_DIM)
+          search = curses.newwin(1, x - 51, 5, 47)
+          edit = curses.newwin(1, x - 46, 9, 41)
+          searchbox = Textbox(search)
+          editbox = Textbox(edit)
+          rectangle(stdscr, 4, 4, 6, x - 4)
+          rectangle(stdscr, 8, 4, 10, x - 4)
+          stdscr.refresh()
+          searchbox.edit()
+          editbox.edit()
+          searchinput = searchbox.gather().replace("\n", "").strip()
+          editinput = editbox.gather().replace("\n", "").strip()
 
+          film = collection.find_one({"name": {"$regex": searchinput, "$options": "i"}})
 
+          if not film:
+            stdscr.addstr(12, 4, "Film nicht gefunden!", COLOR_ERROR)
+          else:
+            if option == "rating":
+              try:
+                editinputfloat = float(editinput)
+                collection.update_one({"_id": film["_id"]}, {"$set": {option: editinputfloat}})
+                stdscr.addstr(12, 4, " Film aktualisiert!", COLOR_GREEN)
+              except ValueError:
+                stdscr.addstr(12, 4, "Error, keine gültige Zahl!", COLOR_ERROR)
+            elif option in ["jahr", "min_alter"]:
+              try:
+                editinputnum = int(editinput)
+                collection.update_one({"_id": film["_id"]}, {"$set": {option: editinputnum}})
+                stdscr.addstr(12, 4, " Film aktualisiert!", COLOR_GREEN)
+              except ValueError:
+                stdscr.addstr(12, 4, "Error, keine gültige Zahl!", COLOR_ERROR)
+            elif option in ["art", "schauspieler"]:
+              collection.update_one({"_id": film["_id"]}, {"$set": {option: [editinput]}})
+              stdscr.addstr(12, 4, " Film aktualisiert!", COLOR_GREEN)
+            else:
+              collection.update_one({"_id": film["_id"]}, {"$set": {option: editinput}})
+              stdscr.addstr(12, 4, " Film aktualisiert!", COLOR_GREEN)
 
   except Exception as e:
     stdscr.addstr(4, 4, "Error:", COLOR_ERROR)
